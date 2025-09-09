2b9eec0fc19c0b57a2214931988d845bd783b63b
fridalif <fridalif2@gmail.com>
2025-09-09 23:56:07 +0300

    Убрана запись клбчей

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,6 @@ fn main() -> Result<(), Box<dyn Error>>{
     let current_configuration = Config::new("../etc/config.bin");
     let (client_private_key, client_public_key) = cryptography::generate_rsa_keys().unwrap();
 
-    std::fs::write("../etc/client_public_key.bin", cryptography::encrypt_config(&client_public_key.as_bytes(), b"01234567890123456789012345678901").unwrap()).expect("Failed to write msg");
-    std::fs::write("../etc/client_private_key.bin", cryptography::encrypt_config(&client_private_key.as_bytes(), b"01234567890123456789012345678901").unwrap()).expect("Failed to write msg");
-    
-
     let real_config = Arc::new(Mutex::new(AppConfig::new(current_configuration, client_private_key, client_public_key)));
 
 
