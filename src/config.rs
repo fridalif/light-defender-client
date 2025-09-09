ae49bb2e82d9a3295e7acaec2d1ac3d3c40b3091
fridalif <fridalif2@gmail.com>
2025-09-09 13:02:14 +0000

    Начата работа над считыванием конфига из файла

--- src/config.rs
+++ src/config.rs
@@ -10,3 +10,14 @@ pub struct Config {
     pub token: String,
     pub connector_address: String,
 }
+
+//pub secret: 6ba7885277793bca54b3c26ee9a6b72a
+impl Config {
+    pub fn new(
+        path: &str,
+    ) -> Self {
+        let config_str = std::fs::read_to_string(path).expect("Failed to read config file");
+        let config: Config = serde_json::from_str(&config_str).expect("Failed to parse config file");
+        config
+    }
+}
