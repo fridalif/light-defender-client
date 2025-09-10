d38b0f8b7250e4e25459deb7a9793262c4db0cf0
fridalif <fridalif2@gmail.com>
2025-09-10 23:26:19 +0300

    функция коннекта к вебсокету

--- src/connector.rs
+++ src/connector.rs
@@ -5,26 +5,27 @@ use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 use crate::config::AppConfig;
 
 pub struct Connector {
-    pub app_config: AppConfig
+    pub app_config: Arc<Mutex<AppConfig>>,
+    pub ws_stream: Option<Arc<Mutex<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>>>
 }
 
 pub trait ConnectorTrait {
-    async fn new(app_config: AppConfig) -> Self;
+    async fn new(app_config: Arc<Mutex<AppConfig>>) -> Self;
     async fn connect(&self) -> Result<Arc<Mutex<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>>, Box<dyn std::error::Error>>;
-    async fn run(&self);
-    async fn serve_websocket(&self, ws_stream: Arc<Mutex<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>>);
+    async fn run(&mut self);
+    async fn serve_websocket(&self);
 }
 
 impl ConnectorTrait for Connector {
-    async fn new(app_config: AppConfig) -> Self {
-        Connector { app_config }
+    async fn new(app_config: Arc<Mutex<AppConfig>>) -> Self {
+        Connector { app_config, ws_stream: None }
     }
 
-    async fn run (&self) {
+    async fn run (&mut self) {
         loop {
             match self.connect().await {
                 Ok(ws_stream) => {
-                    self.serve_websocket(ws_stream).await;
+                    self.ws_stream = Some(ws_stream);
                 }
                 Err(e) => {
                     println!("Failed to connect: {}", e);
@@ -35,11 +36,12 @@ impl ConnectorTrait for Connector {
     }
 
     async fn connect(&self) -> Result<Arc<Mutex<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>>, Box<dyn std::error::Error>> {
-        let (ws_stream, _) = connect_async("ws://localhost:8000").await?;
+        let app_config = self.app_config.lock().map_err(|_| "Failed to lock app config")?;
+        let (ws_stream, _) = connect_async(&*app_config.client_config.connector_address).await?;
         Ok(Arc::new(Mutex::new(ws_stream)))
     }
 
-    async fn serve_websocket(&self, ws_stream: Arc<Mutex<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>>) {
+    async fn serve_websocket(&self) {
         let ws_stream = self.connect().await.unwrap();
         let (mut ws_stream, _) = ws_stream.lock().await.split();
     }
