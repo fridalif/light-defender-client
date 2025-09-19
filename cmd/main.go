996f7fd4f36d016c5f287671ed6033a61aa9d70e
fridalif <fridalif2@gmail.com>
2025-09-19 23:31:05 +0300

    инициализация сервера

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"light-defender-client/internal/connector"
 	"light-defender-client/pkg/config"
+	"log"
 )
 
 func main() {
-	_, err := config.NewConfig()
+	appConfig, err := config.NewConfig()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("config error: %v", err)
+	}
+	server := connector.NewConnector(appConfig)
+
+	err = server.Run()
+	if err != nil {
+		log.Fatalf("server error: %v", err)
 	}
 }
